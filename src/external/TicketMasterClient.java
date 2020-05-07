a32a049bf3540ba2bf5d7ae0914891c03b7f86fe
RyanShang627 <dadong.shang@outlook.com>
2020-05-07 09:48:47 -0700

    fix: Remove the EnvParser class and directly write API key inside the java file

--- src/external/TicketMasterClient.java
+++ src/external/TicketMasterClient.java
@@ -17,7 +17,7 @@ public class TicketMasterClient {
 	private static final String HOST = "https://app.ticketmaster.com";
 	private static final String ENDPOINT = "/discovery/v2/events.json";
 	private static final String DEFAULT_KEYWORD = "event";
-	private static final String API_KEY = EnvParser.getTicketMasterKey();
+	private static final String API_KEY = "IMn4J2NotwtAU62YUidAUuQQH1l2QsCi";
 
 	/**
 	 * This method search the events based on the given latitude, longitude, and
