dc1fa1845e8ac75fc6cf1731f6b4079f017553b9
RyanShang627 <dadong.shang@outlook.com>
2020-05-07 11:35:22 -0700

    feat: Define the event item class

---
+++ src/entity/Item.java
@@ -0,0 +1,20 @@
+package entity;
+
+import java.util.Set;
+
+/**
+ * 
+ * @author Ryan Shang
+ *
+ */
+public class Item {
+	private String itemId;
+	private String name;
+	private double rating;
+	private String address;
+	private Set<String> categories;
+	private String imageUrl;
+	private String url;
+	private double distance;
+
+}
