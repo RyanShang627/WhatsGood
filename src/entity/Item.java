466c46f947e95c7123e36b41a8cc4ea3c813d734
RyanShang627 <dadong.shang@outlook.com>
2020-05-07 11:40:55 -0700

    feat: Generate getters
    To make data fields can be accessed by others, normally we need Getter
    and Setter for each of them. In this case, we just need Getters because
    we don’t want to change an item instance once it’s constructed.

--- src/entity/Item.java
+++ src/entity/Item.java
@@ -3,11 +3,15 @@ package entity;
 import java.util.Set;
 
 /**
+ * This class defines the structure of an event item holding data fields that
+ * are needed in the project
  * 
  * @author Ryan Shang
+ * @date 2020-May-07 11:37:09 AM
  *
  */
 public class Item {
+	// fields of an event item
 	private String itemId;
 	private String name;
 	private double rating;
@@ -16,5 +20,37 @@ public class Item {
 	private String imageUrl;
 	private String url;
 	private double distance;
+
+	public String getItemId() {
+		return itemId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public Set<String> getCategories() {
+		return categories;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
 
 }
