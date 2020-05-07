8998deb0f0dfbf97c8b2992244983fbe96c05b41
RyanShang627 <dadong.shang@outlook.com>
2020-05-06 23:15:33 -0700

    feat: Use JSON as response type
    Use JSON instead of html as the response type within the SearchItem
    servlet.

--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -9,44 +9,57 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Servlet implementation class SearchItem
  */
 @WebServlet("/search")
 public class SearchItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SearchItem() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		PrintWriter writer = response.getWriter();
-		
-		if (request.getParameter("username") != null) {
-			String username = request.getParameter("username");
-			
-			writer.println("<html><body>");
-			writer.println("<h1>Hello " + username + "</h1>");
-			writer.println("</body></html>");
-		}
-
-		
-		writer.close();	
+	public SearchItem() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("application/json");
+		PrintWriter writer = response.getWriter();
+
+		if (request.getParameter("username") != null) {
+			String username = request.getParameter("username");
+
+			JSONObject resContent = new JSONObject();
+
+			try {
+				resContent.put("username", username);
+			} catch (JSONException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+
+			writer.print(resContent);
+		}
+
+		writer.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
