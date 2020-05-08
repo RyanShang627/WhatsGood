9b639ab492d63eb6bc8736955de175e140171631
RyanShang627 <dadong.shang@outlook.com>
2020-05-08 12:09:46 -0700

    feat: Implement the MySQLConnection constructor

--- src/db/mysql/MySQLConnection.java
+++ src/db/mysql/MySQLConnection.java
@@ -1,12 +1,38 @@
 package db.mysql;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.List;
 import java.util.Set;
 
 import db.DBConnection;
 import entity.Item;
 
+/**
+ * This class implements DBConnection interface and provides a set of behaviors
+ * to talk to MySQL database
+ * 
+ * @author Ryan Shang
+ * @date 2020-May-08 12:03:40 PM
+ *
+ */
 public class MySQLConnection implements DBConnection {
+
+	private Connection conn;
+
+	/**
+	 * This class constructor helps connect to MySQL database
+	 */
+	public MySQLConnection() {
+		try {
+			// Allow the JVM to recognize and load the MySQL jdbc driver
+			Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
+			// Build MySQL connection
+			conn = DriverManager.getConnection(MySQLDBUtil.URL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Override
 	public void close() {
