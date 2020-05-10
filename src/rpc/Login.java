9380e962ba5c41d22ee4fdbf1144400971179b42
RyanShang627 <dadong.shang@outlook.com>
2020-05-10 15:49:09 -0700

    feat: doGet method of Login servlet
    Implement the doGet method of Login servlet

--- src/rpc/Login.java
+++ src/rpc/Login.java
@@ -1,11 +1,18 @@
 package rpc;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONObject;
+
+import db.DBConnection;
+import db.DBConnectionFactory;
 
 /**
  * Servlet implementation class Login
@@ -13,27 +20,54 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		DBConnection connection = DBConnectionFactory.getConnection();
+
+		try {
+			// Fetch HTTP session from the request.
+			// Session is null when the session doesn't exist
+			HttpSession session = request.getSession(false);
+
+			JSONObject obj = new JSONObject();
+
+			if (session != null) {
+				String userId = session.getAttribute("user_id").toString();
+				// prepare the response body
+				obj.put("status", "OK").put("user_id", userId).put("name", connection.getFullname(userId));
+			} else {
+				obj.put("status", "Invalid Session");
+				response.setStatus(403);
+			}
+			// write the JSON object to the response
+			RpcHelper.writeJSONObject(response, obj);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// close the connection
+			connection.close();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
